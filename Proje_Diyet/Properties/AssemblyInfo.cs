802ed0a7ee42d15f5fa7a22a3ea085020b12b996
yukselagagisi <yukselagagisi@gmail.com>
2024-03-08 18:11:15 +0300

    Minor bug fixes

--- Proje_Diyet/Properties/AssemblyInfo.cs
+++ Proje_Diyet/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Proje_Diyet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A program developed with C# and Sqlite to meet the data storage needs of dietitians.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Proje_Diyet")]
 [assembly: AssemblyProduct("Proje_Diyet")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Proje_Diyet")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
